45df624cbea9de1b83eca317ee7c72bd771da18c
Kerry Mahne <kerry.mahne@tretton37.com>
2020-04-11 13:14:54 +0200

    added middleware support

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+function compose(...funcs) {
+  return funcs.reduce((previous, next) => (...args) => previous(next(...args)));
+}
+
+const loggerMiddleware = (arg1) => (next) => (action) => {
+  // arg1 is the API, next is the next function, action is what we received
+  console.log(`%c${action.type}`, 'background: orange', action);
+  next(action);
+};
+
+const stateLoggerMiddleware = ({getState}) => (next) => (action) => {
+  console.log('%cState', 'background: green', getState());
+  next(action);
+};
+
 export function useStore({reducer, initialState, windowName}) {
   const [state, dispatch] = React.useReducer(reducer, initialState);
   windowName && (window[`$${windowName}`] = state);
 
-  return {state, dispatch};
+  // helpers to get state and dispatch inside your middleware
+  const middlewareAPI = {
+    getState: () => state,
+    dispatch: (action) => dispatch(action),
+  };
+
+  const middlewares = [loggerMiddleware, stateLoggerMiddleware];
+  const chain = middlewares.map((middleware) => middleware(middlewareAPI));
+  const enhancedDispatch = compose(...chain)(dispatch);
+
+  return {state, dispatch: enhancedDispatch};
 };
 
