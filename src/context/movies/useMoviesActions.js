0ca0652172459d191e43296b519f80d36673f26b
Jure Stepisnik <jure.stepisnik@tretton37.com>
2021-12-18 13:59:46 +0100

    Updated api url

--- src/context/movies/useMoviesActions.js
+++ src/context/movies/useMoviesActions.js
@@ -1,36 +1,32 @@
-import {useMoviesState} from '.';
+import { useMoviesState } from ".";
 import {
   SET_MOVIES_LOADING,
   SET_MOVIES_LIST,
   SET_MOVIES_ERROR,
-} from './actionTypes';
+} from "./actionTypes";
 
 const apiKey = process.env.API_KEY;
-const url = 'https://the-one-api.herokuapp.com/v1';
+const url = "https://the-one-api.dev/v2";
 
 export function useMoviesActions() {
-  const {dispatch} = useMoviesState();
+  const { dispatch } = useMoviesState();
 
   return {
     fetchMovies: async () => {
-      dispatch({type: SET_MOVIES_LOADING, payload: true});
+      dispatch({ type: SET_MOVIES_LOADING, payload: true });
       try {
-        const response = await fetch(
-            `${url}/movie`,
-            {
-              method: 'GET',
-              headers: {
-                Authorization: `Bearer ${apiKey}`,
-              },
-            },
-        );
+        const response = await fetch(`${url}/movie`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+          },
+        });
         const json = await response.json();
-        dispatch({type: SET_MOVIES_LIST, payload: json.docs});
+        dispatch({ type: SET_MOVIES_LIST, payload: json.docs });
       } catch (err) {
-        console.error('Error while fetching movies', err);
-        dispatch({type: SET_MOVIES_ERROR, paylod: err});
+        console.error("Error while fetching movies", err);
+        dispatch({ type: SET_MOVIES_ERROR, paylod: err });
       }
     },
   };
 }
-
