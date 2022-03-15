e0170a1886037084fb7c3b1365e041ee1592af84
Kerry Mahne <kerry.mahne@gmail.com>
2022-03-15 11:10:47 +0100

    basename to router ,gh pages works :D

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const AppStyled = styled.div`
 
 const Application = () => {
   return (
-    <Router>
+    <Router basename="/hooksStateMan">
       <AppStyled>
         <GlobalStyle />
         <Header />
